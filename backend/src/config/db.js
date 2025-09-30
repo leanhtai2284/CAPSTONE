c103c093f1e82bf01de55bc89f97fd81d7a497aa
rum <134195166+leanhtai2284@users.noreply.github.com>
2025-09-30 23:28:30 +0700

    commit register

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,9 +6,9 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(`✅ MongoDB connected: ${conn.connection.host}`);
+    console.log(` MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`❌ MongoDB connection failed: ${error.message}`);
+    console.error(` MongoDB connection failed: ${error.message}`);
     process.exit(1);
   }
 };
